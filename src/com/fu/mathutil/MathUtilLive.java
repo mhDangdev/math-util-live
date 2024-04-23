083eb22588c088b6a01390ab7eec3cc502951b15
mdDangdev <haidang18101810@gmail.com>
2024-04-24 01:26:25 +0700

    sua code lan 2

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -6,8 +6,8 @@ public class MathUtilLive {
 
 
     public static void main(String[] args) {
-        System.out.print("6! = ");
-        System.out.println(MathUtil.getFactorial(6));
+        System.out.print("9! = ");
+        System.out.println(MathUtil.getFactorial(9));
     }
     
 }
